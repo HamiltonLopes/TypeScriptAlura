c6f290344734c77a8ca674ca6c0d99180f4bb57f
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-09-22 23:09:58 -0300

    Class 3 finished

---
+++ app/controllers/Negociacao-controller.ts
@@ -0,0 +1,33 @@
+import Negociacao from "../models/Negociacao.js";
+
+export default class NegociacaoController {
+
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+
+    constructor(){
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+
+    adiciona(): void{
+        const negociacao: Negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    }
+
+    criaNegociacao(): Negociacao{
+        const exp = /-/g;
+        const date = new Date (this.inputData.value.replace(exp, ','));
+        return new Negociacao(date, parseInt(this.inputQuantidade.value), parseFloat(this.inputValor.value));
+    }
+
+    limparFormulario(): void{
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,24 @@
+import Negociacao from "../models/Negociacao.js";
+export default class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        return new Negociacao(date, parseInt(this.inputQuantidade.value), parseFloat(this.inputValor.value));
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
